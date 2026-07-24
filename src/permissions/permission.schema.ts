fbc29f7bdb3d740df51bd16109e449f4110058ad
Astha346 <bhattastha70@gmail.com>
2026-07-24 17:14:56 +0545

    feat(rbac): add permissions module

---
+++ src/permissions/permission.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type PermissionDocument = Permission & Document;
+
+@Schema({
+  timestamps: true,
+})
+export class Permission {
+  @Prop({
+    required: true,
+    unique: true,
+    trim: true,
+  })
+  name!: string;
+
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  module!: string;
+
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  description!: string;
+}
+
+export const PermissionSchema =
+  SchemaFactory.createForClass(Permission);
